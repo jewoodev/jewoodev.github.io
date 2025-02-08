01a9ade289bcfc2addba7d2d64073762fb496eab
jewoodev <jewoos15@naver.com>
2025-02-08 19:38:59 +0900

    fix: File cleanup for issue resolution

--- commitlint.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "subject-case": [0, "always"], // 한글 허용
-    "header-max-length": [2, "always", 100] // 제목 길이 늘리기
-  }
-};
